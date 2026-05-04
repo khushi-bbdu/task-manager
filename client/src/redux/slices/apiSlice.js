628633014c962f99a7bbf9c7ddac9b69a72d2a27
Khushi Tripathi <login4khushitripathi@gmail.com>
2026-05-04 19:16:38 +0530

    updated API URL for production

--- client/src/redux/slices/apiSlice.js
+++ client/src/redux/slices/apiSlice.js
@@ -1,6 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const API_URI = "http://localhost:8800/api";
+//const API_URI = "http://localhost:8800/api";
+
+const API_URI = "task-manager-production-8421.up.railway.app";
 
 const baseQuery = fetchBaseQuery({ baseUrl: API_URI });
 
